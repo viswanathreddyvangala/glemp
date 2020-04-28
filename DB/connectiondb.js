3cc3f1ae7de894c48c6e853a3dac7b64ae2b2c3e
viswanathreddyvangala <v.viswa000@gmail.com>
2020-04-28 21:21:27 +0530

    new changes

--- DB/connectiondb.js
+++ DB/connectiondb.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const URI = "mongodb+srv://viswareddy:viswareddy@cluster0-yeyph.gcp.mongodb.net/glempdata?retryWrites=true&w=majority";
 
 const connectDB = async() =>{
-    await mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true  });
-    console.log(" DB connected ...")
+    await mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true  },(err,db)=>{
+       if(err){
+           console.log("error",err)
+       }
+       else{
+           console.log("connected to mongoDB")
+       }
+    });
 }
 module.exports = connectDB;
