8a3c9bca2989c8fc8a62219376c32eada4f2516f
viswanathreddyvangala <v.viswa000@gmail.com>
2020-04-28 21:06:57 +0530

    new cahage

--- DB/connectiondb.js
+++ DB/connectiondb.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = "mongodb+srv://glemp:glemp@cluster0-cre4j.mongodb.net/glempdata?retryWrites=true&w=majority";
+const URI = "mongodb+srv://viswareddy:viswareddy@cluster0-yeyph.gcp.mongodb.net/glempdata?retryWrites=true&w=majority";
+
 const connectDB = async() =>{
     await mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true  });
     console.log(" DB connected ...")
